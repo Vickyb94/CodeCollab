16608eceb9b81669e28e59a6622cc785003c4ac9
Stephen Brown <stephenrbrownnb@gmail.com>
2023-02-07 21:30:54 -0400

    Added sql connection

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
-
 const express = require('express');
 const routes = require('./controllers');
-
+require('dotenv').config();
+const mysql = require('mysql2');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(routes);
+
+const db = mysql.createConnection(
+   
+    {
+      host: 'localhost',
+      // MySQL Username
+      user: process.env.DB_USER,
+      // TODO: Add MySQL Password
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    },
+);
 
 app.listen(PORT, () =>
 console.log(`Listening at http://localhost:${PORT}`)
