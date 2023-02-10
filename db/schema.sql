c9c8aff06316870ee6a4f418fa1a8382d929a337
Stephen Brown <stephenrbrownnb@gmail.com>
2023-02-09 20:38:21 -0400

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,8 +4,27 @@ CREATE DATABASE db_codeCollab;
 USE db_codeCollab;
 
 CREATE TABLE user (
-  userId INT NOT NULL,
+  userId INT NOT NULL AUTO_INCREMENT,
+  userName VARCHAR(30) NOT NULL,
   userEmail VARCHAR(30) NOT NULL,
   userPassword VARCHAR(30) NOT NULL,
   PRIMARY KEY (userId)
 );
+
+CREATE TABLE languages (
+  userID INT NOT NULL,
+  JavaScript INT,
+  Python INT,
+  Java INT,
+  Swift INT,
+  FOREIGN KEY (userId) REFERENCES user(userId)
+);
+
+CREATE TABLE posts (
+ postId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+ content VARCHAR(30) NOT NULL,
+ imageLink VARCHAR(30) NOT NULL,
+ postUser_id INT,
+ FOREIGN KEY (postUser_id) REFERENCES user(userId)
+);
+
