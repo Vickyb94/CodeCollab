3d162ab224ebf97aa273c731db2028f50666de89
Stephen Brown <stephenrbrownnb@gmail.com>
2023-02-09 20:03:49 -0400

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,10 @@
 DROP DATABASE IF EXISTS db_codeCollab;
 CREATE DATABASE db_codeCollab;
 USE db_codeCollab;
+
+CREATE TABLE user (
+  userId INT NOT NULL,
+  userEmail VARCHAR(30) NOT NULL,
+  userPassword VARCHAR(30) NOT NULL,
+  PRIMARY KEY (userId)
+);
