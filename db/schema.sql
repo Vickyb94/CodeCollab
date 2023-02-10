7c8539af311bb3b42ebdf2573fcaca8724f842a5
Stephen Brown <stephenrbrownnb@gmail.com>
2023-02-09 20:02:40 -0400

    Added users.sql and tables plan

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,4 @@
 
 DROP DATABASE IF EXISTS db_codeCollab;
 CREATE DATABASE db_codeCollab;
+USE db_codeCollab;
---
+++ seeds/users.sql
@@ -0,0 +1,6 @@
+CREATE TABLE user (
+  userId INT NOT NULL,
+  userEmail VARCHAR(30) NOT NULL,
+  userPassword VARCHAR(30) NOT NULL,
+  PRIMARY KEY (userId)
+);
