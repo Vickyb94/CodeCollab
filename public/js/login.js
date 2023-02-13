66be591c4a9e06c23f8212241aff9435cba0a6e9
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2023-02-13 06:21:16 -0400

    wrote a function to take user input and initiate login process

---
+++ public/js/login.js
@@ -0,0 +1,23 @@
+const loginFormHandler = async function(event) {
+    event.preventDefault();
+
+    // collect username input and password input from login form
+     const username = document.querySelector("#username-input").value.trim();
+     const password = document.querySelector("#password-input").value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ username, password}),
+        headers: { 'Content-Type': 'application/json'},
+        });
+        //redirect browser to dashboard
+        if (response.ok) {
+          document.location.replace('/dashboard');
+        }
+          else {
+          alert('Login Failed! Please try again');
+        }
+    }
+
+};
