92b419ad1dc5b7052784cad6004e48e077f00d44
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2023-02-13 05:06:15 -0400

    added comments to logout.js

--- public/js/logout.js
+++ public/js/logout.js
@@ -3,13 +3,14 @@ const logout = async () => {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
     });
-
+//change path if log out was succesful
     if (response.ok) {
         document.location.replace('/');    
     } 
+    //alert user if log out failed
     else {
         alert ('Error! Please try logging out again.');
     }
 };
-
+//add event listener 
 document.querySelector('#logout').addEventListener('click', logout);
