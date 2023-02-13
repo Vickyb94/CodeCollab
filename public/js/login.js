34f5ab0059be699131bcc60f10485ce6f7efbf3e
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2023-02-13 19:20:16 -0400

    updated login.js

--- public/js/login.js
+++ public/js/login.js
@@ -6,7 +6,7 @@ const loginFormHandler = async function(event) {
      const password = document.querySelector("#password-input").value.trim();
 
     if (username && password) {
-        const response = await fetch('/api/users/login', {
+        const response = await fetch('/api/login', {
         method: 'POST',
         body: JSON.stringify({ username, password}),
         headers: { 'Content-Type': 'application/json'},
--- public/js/signup.js
+++ public/js/signup.js
@@ -6,7 +6,7 @@ const signupFormHandler = async (event) => {
     const password = document.querySelector('#password-input-signup').value.trim();
 
     if(username && email && password) {
-        const response = await fetch('/api/users/login', {
+        const response = await fetch('/api/signup', {
             method: 'POST',
             body: JSON.stringify({ username, email, password}),
             headers: { 'Content-Type': 'application/json'},
