49ea3dea81e118acbb43f250a3b1d0f9968d2b6e
Gabe Morris <gabemorris1042@gmail.com>
2023-02-14 20:25:20 -0400

    added user id to session storage for signup route

--- utils/loggedIn.js
+++ utils/loggedIn.js
@@ -1,8 +1,9 @@
 
 // middleware function to check the loggedIn status of a user
 const loggedIn = (req, res, next) => {
+  console.log("loggedIn middleware")
   // if status is true, next
-  if (req.session.loggedIn) {next()}
+  if (req.session.logged_in) {next()}
   // if status is false, redirect user to login handlebar
   else {res.redirect('/login')}
 }
