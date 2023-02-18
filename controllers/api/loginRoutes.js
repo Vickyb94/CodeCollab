44c3b1226c689da0781d9445b8f086ed886df36a
Stephen Brown <stephenrbrownnb@gmail.com>
2023-02-18 17:55:12 -0400

    Testing again

--- controllers/api/loginRoutes.js
+++ controllers/api/loginRoutes.js
@@ -37,4 +37,4 @@ router.post('/', async (req, res) => {
   }
 })
 
-module.exports = router, loggedInUser ;
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -7,7 +7,6 @@ const { Post, User, Language } = require('../models')
 const loggedIn = require('../utils/loggedIn');
 
 // get the posts for the dashboard
-let allPosts;
 router.get('/', async (req, res) => {
 
   const postData = await Post.findAll({
@@ -20,7 +19,7 @@ router.get('/', async (req, res) => {
   });
 
   // put postData into an array
-  allPosts = postData.map((post) => post.get({ plain: true }));
+  const allPosts = postData.map((post) => post.get({ plain: true }));
 
   // render dashboard with allPosts
   res.render('dashboard', {
@@ -47,4 +46,4 @@ router.get(`/logout`, async (req,res) => {
 //   res.render(`dashboard`, {});
 // });    
 
-module.exports = router, allPosts;
+module.exports = { router };
