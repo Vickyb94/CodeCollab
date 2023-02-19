8cfbe1432ee9ea79485e59ac2d2fd928846f6bb2
Gabe Morris <gabemorris1042@gmail.com>
2023-02-19 14:31:19 -0400

    added some more users to seeds, as well as some actual coding related posts instead of just random pictures and text

--- seeds/seedPosts.js
+++ seeds/seedPosts.js
@@ -3,9 +3,19 @@ const { Post } = require('../models');
 
 const postData = [
     {
-        content: "A cool wallpaper I found.",
-        imageLink: "https://res.cloudinary.com/dbzzfnp6n/image/upload/v1675990167/cld-sample-4.jpg",
+        content: "Just started using Inquirer!",
+        imageLink: "https://res.cloudinary.com/dbzzfnp6n/image/upload/v1676831091/inquirer-code_kh0hvl.png",
         userId: 1
+    },
+    {
+        content: "Created a session for user authentication for my app!",
+        imageLink: "https://res.cloudinary.com/dbzzfnp6n/image/upload/v1676831091/session-code_lgbb4e.png",
+        userId: 2
+    },
+    {
+        content: "Does anyone know if theres an easier way to do this? My hands are getting tired...",
+        imageLink: "https://res.cloudinary.com/dbzzfnp6n/image/upload/v1676831092/console-logging_gmjft8.png",
+        userId: 3
     },
 ];
 
